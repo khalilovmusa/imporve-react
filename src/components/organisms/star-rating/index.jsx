6bd3a363d7b725487362920e8682e3ff53459c87
Musa <Musaxlilov29@gmail.com>
2025-03-17 04:58:52 +0400

    Star rating

---
+++ src/components/organisms/star-rating/index.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { FaStar } from 'react-icons/fa'
+import './styles.css'
+
+const StarRating = ({amountOfStars = 5}) => {
+
+    const [ rating, setRating ] = useState(0)
+    const [ hover, setHover ] = useState(0)
+
+    const handleClick = (getCurrentIndex) => {
+        setRating(getCurrentIndex)
+    }
+
+    const handleMouseEnter = (getCurrentIndex) => {
+        setHover(getCurrentIndex)
+    }
+
+    const handleMouseLeave = () => {
+       setHover(rating)
+    }
+
+    return(
+        <div style={{ display: 'flex', height: '100vh', width: '100vw' }}>
+            {
+                [...Array(amountOfStars)].map((_, index) => {
+                    index += 1
+                    return <FaStar 
+                    key={index}
+                    className={index <= (hover || rating) ? 'active' : 'inactive'}
+                    onClick={() => handleClick(index)}
+                    onMouseEnter={() => handleMouseEnter(index)}
+                    onMouseLeave={() => handleMouseLeave(index)}
+                    size={40}
+                    />
+                })
+            }
+        </div>
+    )
+}
+
+export default StarRating
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import React from 'react'
 import Accordion from './components/organisms/accordion'
-import RandomColor from './components/organisms/random-color'
+import RandomColor from './components/organisms/star-rating/random-color'
+import StarRating from './components/organisms/star-rating'
 
 createRoot(document.getElementById('root')).render(
   <>
    <Accordion />
    <RandomColor />
+   <StarRating amountOfStars={10} />
   </>
 )
