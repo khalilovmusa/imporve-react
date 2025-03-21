c18dd746b4157a444b819604ea929e6be13602a4
Musa <Musaxlilov29@gmail.com>
2025-03-21 11:58:35 +0400

    image slider add

---
+++ src/components/organisms/image-slider/index.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react'
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs'
+import './styles.css'
+
+const ImageSlider = ({ url, limit = 5, page = 1 }) => {
+
+    const [ images, setImages ] = useState([]);
+    const [ currentSlide, setCurrentSlide ] = useState(0);
+    const [ errorMsg, setErrorMsg ] = useState(null);
+    const [ loading, setLoading ] = useState(false);
+
+    async function fetchImages (getUrl){
+        try{
+            setLoading(true)
+            const res = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
+            const data = await res.json()
+
+            if(data){
+                setImages(data)
+                setLoading(false)
+            }
+
+        }catch(e){
+            setErrorMsg(e.message)
+            setLoading(false)
+        }
+    }
+
+    const handlePrevious = () => {
+        setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1)
+    }
+
+    const handleNext = () => {
+        setCurrentSlide(currentSlide === images.length ? 0 : currentSlide + 1)
+    }
+
+    console.log(images)
+
+    useEffect(() => {
+        if(url !== '') fetchImages(url)
+    },[url])
+
+    if(loading) {
+        return <div>Loading...</div>
+    }
+
+    if(errorMsg !== null) {
+        return <div>An error ocurred {errorMsg}</div>
+    }
+
+    return(
+        <div className='image-container'>
+            <BsArrowLeftCircleFill onClick={handlePrevious} className='arrow arrow-left' />
+            {
+                images && images.length ?
+                images.map((imageItem , index) => (
+                    <img 
+                    key={imageItem.id} 
+                    alt={imageItem.download_url} 
+                    src={imageItem.download_url} 
+                    className={currentSlide === index ? 'current-image' : 'current-image hide-current-image'}
+                    />
+                ))
+            : null }
+            <span className='circle-indicators'>
+                {
+                    images && images.length ? images.map((_, index) => (
+                        <button
+                         key={index} 
+                        className={ currentSlide === index ? 'current-indicator' : 'current-indicator inactive-indicator'}
+                        onClick={() => setCurrentSlide(index)}
+                        >
+                        </button> )) : null
+                }
+            </span>
+            <BsArrowRightCircleFill onClick={handleNext} className='arrow arrow-right' />
+        </div>
+    )
+}
+
+export default ImageSlider
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import React from 'react'
 import Accordion from './components/organisms/accordion'
-import RandomColor from './components/organisms/star-rating/random-color'
+import RandomColor from './components/organisms/random-color'
 import StarRating from './components/organisms/star-rating'
+import ImageSlider from './components/organisms/image-slider'
 
 createRoot(document.getElementById('root')).render(
   <>
    <Accordion />
+
    <RandomColor />
+
    <StarRating amountOfStars={10} />
+
+   <ImageSlider url={'https://picsum.photos/v2/list'} limit={10} page={'1'}/>
   </>
 )
